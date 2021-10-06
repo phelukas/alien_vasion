de6c07dab8fd87621ad5b9f74bea50f714d6862f
phelukas <ppedrolukas19@gmail.com>
2021-10-05 21:21:30 -0300

    alienigena: atirando nos aliens

--- settings.py
+++ settings.py
@@ -13,14 +13,15 @@ class Settings():
         self.ship_speed_factor = 1.5
         
         # Configuração dos projéteis
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
-        self.bullet_allowed = 3
+        self.bullet_allowed = 10
 
         # Configuração dos alienígenas
         self.alien_speed_factor = 0.05
         self.fleet_drop_speed = 10
+        
         # fleet_direction igual a 1 representa a direita; -1 representa a esquerda
         self.fleet_direction = 1
